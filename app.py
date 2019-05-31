7dd5340dfd611bd9033eda7c6dc2db958b2e5f96
pafable <philafable@gmail.com>
2019-05-31 18:57:08 -0400

    added flask_bootstrap

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from kubernetes import client, config
 from Kubefront import Kubefront
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
